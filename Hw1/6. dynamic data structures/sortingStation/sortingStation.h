a8763637526f7c22208466b4209e7ba075405019
Artem Nesterenko <hohol.amogus@mail.ru>
2023-10-28 00:23:15 +0500

    English text

---
+++ Hw1/6. dynamic data structures/sortingStation/sortingStation.h
@@ -0,0 +1,10 @@
+#pragma once
+
+//Prioritizes operations
+int priority(char symbol);
+
+//Converts an expression to postfix form
+void stringProcessing(char string[], char finalString[], int lengthString);
+
+//Test for program functions
+int test();
