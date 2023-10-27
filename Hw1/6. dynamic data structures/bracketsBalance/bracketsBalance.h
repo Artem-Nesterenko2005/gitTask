9c78fdaa68ca817c0b93147eef090445b9d3fffc
Artem Nesterenko <hohol.amogus@mail.ru>
2023-10-27 23:48:15 +0500

    English text

--- Hw1/6. dynamic data structures/bracketsBalance/bracketsBalance.h
+++ Hw1/6. dynamic data structures/bracketsBalance/bracketsBalance.h
@@ -1,10 +1,10 @@
 #pragma once
 
-//Меняет закрывавшую скобку на открывавшую скобку
+//Changes a closing parenthesis to an opening parenthesis
 char changeBrackets(char symbol);
 
-//Проверяет баланс скобок в строке
+//Checks the balance of parentheses in a string
 int bracketsBalance(char string[], int length);
 
-//Тесты для bracketsBalance и changeBrackets
+//Tests for bracketsBalance and changeBrackets
 int test();
