45e87cc286f11e45595de868c080979b4f466cb4
Artem Nesterenko <hohol.amogus@mail.ru>
2023-10-27 23:37:03 +0500

    Test for stack

---
+++ Hw1/6. dynamic data structures/testStack/testStack.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdbool.h>
+
+//Тест функции push для стека
+bool testPushAndTopStack();
+
+//Тест функции pop для стека
+bool testPopAndTopStack();
+
+//Тест функции isEmpty для стека
+bool testIsEmpty();
+
+//Итоговый тест всех функций
+bool testStack();
