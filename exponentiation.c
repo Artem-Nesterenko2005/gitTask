274fbcd13e6b74f453f1bf368316254951957869
Artem-Nesterenko2005 <144232708+Artem-Nesterenko2005@users.noreply.github.com>
2023-11-25 00:19:06 +0500

    exponentiation
    added raising to a negative power, fixed tests

--- exponentiation.c
+++ exponentiation.c
@@ -2,41 +2,41 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define LIMITATION 0
-#define END 0 
+#define OK_CODE 0 
 #define ERROR_TEST -1
 
 static double iteration(double number, int degree)
 {
-    if (degree >= 0)
+    bool sign = degree < 0;
+    degree = abs(degree);
+    
+    double result = 1;
+    for (degree; degree > 0; --degree)
     {
-        double result = 1;
-        for (degree; degree > LIMITATION; --degree)
-        {
-            result = result * number;
-        }
-        return result;
+        result = result * number;
     }
-    else
-    {
-        return 0;
-    }
+    return sign && result != 0 ? 1 / result : result;
 }
 
 static double exponentiationLog(double number, int degree)
 {
+    bool sign = degree < 0;
+    degree = abs(degree);
+
     if (degree == 0)
     {
         return 1;
     }
+
     if (degree % 2 == 0)
     {
         double halfPower = exponentiationLog(number, degree / 2);
-        return halfPower * halfPower;
+        return sign ? 1 / (halfPower * halfPower) : halfPower * halfPower;
     }
+
     else
     {
-        return  exponentiationLog(number, degree - 1) * number;
+        return sign ? 1 / (exponentiationLog(number, degree - 1) * number) : (exponentiationLog(number, degree - 1) * number);
     }
 }
 
@@ -44,16 +44,12 @@ static bool test(void)
 {
     double number = 111;
     double degree = 11;
-    if (iteration(number, degree)&& exponentiationLog(number, degree, 1))
-    {
-        return true;
-    }
-    return false;
+    return iteration(number, degree) == exponentiationLog(number, degree);
 }
 
 int main(void)
 {
-    if (test() != true)
+    if (!test())
     {
         printf("Error test");
         return ERROR_TEST;
@@ -61,18 +57,12 @@ int main(void)
     double number = 0;
     printf("Enter number ");
     scanf_s("%lf", &number);
-    int degree = 0;
-    printf("Enter a positive degree ");
-    scanf_s("%d", &degree);
-    double iterationResult = iteration(number, abs(degree));
-    double logNResult = exponentiationLog(number, abs(degree));
-    if (degree < 0)
-    {
-        iterationResult = 1 / iterationResult;
-        logNResult = 1 / logNResult;
-    }
-    printf("Result of a program running in linear time %lf\n", iterationResult);
-    printf("Result of a program running in logarithmic time %lf", logNResult);
-    return END;
-}
 
+    int degree = 0;
+    printf("Enter degree ");
+    scanf_s("%d", &degree);
+
+    printf("Result of a program running in linear time %lf\n", iteration(number, degree));
+    printf("Result of a program running in logarithmic time %lf", exponentiationLog(number, degree));
+    return OK_CODE;
+}
