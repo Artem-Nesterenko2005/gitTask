759b21faa4d94a7e9cf135c0bb3286a6ed6cd18f
Artem-Nesterenko2005 <144232708+Artem-Nesterenko2005@users.noreply.github.com>
2023-11-30 01:27:28 +0500

    binaryRepresentation
    add function "testCase", add const

--- binaryRepresentation.c
+++ binaryRepresentation.c
@@ -1,53 +1,39 @@
 ﻿#include <stdio.h>
 #include <locale.h>
-#include <malloc.h>
 #include <math.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
-#define NUMBER_OF_DIGITS 8
-#define LIMITATION 255
+#define NUMBER_OF_DIGITS 32
 #define ERROR_TEST -1
-#define BEYOND_LIMIT -2
 
-static void binaryRepresentation(char number, int* const array)
+static void binaryRepresentation(int number, int* const array)
 {
-    bool numberSign = 0;
-    numberSign = number < 0 ? 0 : 1;
-    number = numberSign == 1 ? number : abs(number) - 1;
-    for (size_t i = NUMBER_OF_DIGITS; i > 0; --i)
+    bool const numberSign = number < 0;
+    number = !numberSign ? number : abs(number) - 1;
+    for (size_t i = NUMBER_OF_DIGITS + 1; --i > 0;)
     {
         array[i - 1] = number % 2;
         number /= 2;
     }
-    if (!numberSign)
+    if (numberSign)
     {
-        for (size_t i = NUMBER_OF_DIGITS; i > 0; --i)
+        for (size_t i = NUMBER_OF_DIGITS + 1; --i > 0;)
         {
             array[i - 1] = array[i - 1] == 1 ? 0 : 1;
         }
     }
 }
 
-static void columnAddition(int* const array1, int* const array2, int* const sumArray)
+static void binarySum(int* const array1, int* const array2, int* const sumArray)
 {
     size_t numberTransfer = 0;
-    for (size_t i  = NUMBER_OF_DIGITS; i > 0; --i)
+    for (size_t i  = NUMBER_OF_DIGITS + 1; --i > 0;)
     {
         sumArray[i - 1] = array1[i - 1] + array2[i - 1] + numberTransfer;
-        if (sumArray[i - 1] == 2)
-        {
-            numberTransfer = sumArray[i - 1] / 2;
-            sumArray[i - 1] %= 2;
-            continue;
-        }
-        if (sumArray[i - 1] == 3)
-        {
-            numberTransfer = sumArray[i - 1] / 2;
-            sumArray[i - 1] %= 2;
-            continue;
-        }
-        numberTransfer = 0;
+        numberTransfer = sumArray[i - 1] / 2;
+        sumArray[i - 1] %= 2;
     }
 }
 
@@ -61,61 +47,59 @@ static int decimalRepresentation(int* const arrayWithBinaryRepresentation)
     return decimalRepresentation;
 }
 
+static bool testCase(const size_t number1, const size_t number2, int* const right1, int* const right2)
+{
+    int const testArray1[NUMBER_OF_DIGITS] = { 0 };
+    int const testArray2[NUMBER_OF_DIGITS] = { 0 };
+
+    binaryRepresentation(number1, testArray1);
+    binaryRepresentation(number2, testArray2);
+
+    if ((memcmp(testArray1, right1, NUMBER_OF_DIGITS) != 0) || (memcmp(testArray2, right2, NUMBER_OF_DIGITS) != 0))
+    {
+        return false;
+    }
+
+    int const testSum[NUMBER_OF_DIGITS] = { 0 };
+    binarySum(testArray1, testArray2, testSum);
+    int const rightSum[NUMBER_OF_DIGITS] = { 0 };
+    binaryRepresentation(number1 + number2, rightSum);
+    if (memcmp(testSum, rightSum, NUMBER_OF_DIGITS) != 0)
+    {
+        return false;
+    }
+
+    if (decimalRepresentation(testArray1) != number1 || decimalRepresentation(testArray2) != number2)
+    {
+        return false;
+    }
+
+    return true;
+}
+
 static bool test(void)
 {
-    size_t firstTestNumber1 = 5;
-    int firstTestArray1[NUMBER_OF_DIGITS] = { 0 };
+    const size_t firstTestNumber1 = 5;
+    const size_t firstTestnumber2 = 3;
 
-    size_t firstTestnumber2 = 3;
-    int firstTestArray2[NUMBER_OF_DIGITS] = { 0 };
+    int const rightArray1[NUMBER_OF_DIGITS] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1};
+    int const rightArray2[NUMBER_OF_DIGITS] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1};
 
-    binaryRepresentation(firstTestNumber1, firstTestArray1);
-    binaryRepresentation(firstTestnumber2, firstTestArray2);
-    if (firstTestArray1[5] != 1 || firstTestArray1[7] != 1 || firstTestArray2[6] != 1 || firstTestArray2[7] != 1)
+    if (!testCase(firstTestNumber1, firstTestnumber2, rightArray1, rightArray2))
     {
-        printf("Error test number 1\n");
+        printf("Ошибка в тесте номер 1\n");
         return false;
     }
 
-    int firstTestSumArray[NUMBER_OF_DIGITS] = { 0 };
-    columnAddition(firstTestArray1, firstTestArray2, firstTestSumArray);
-    if (firstTestSumArray[4] != 1)
+    int const secondTestNumber1 = -5;
+    int const secondTestnumber2 = -10;
+
+    int const rightArray3[NUMBER_OF_DIGITS] = { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1 };
+    int const rightArray4[NUMBER_OF_DIGITS] = { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,0 };
+    
+    if (!testCase(secondTestNumber1, secondTestnumber2, rightArray3, rightArray4))
     {
-        printf("Error test number 1\n");
-        return false;
-    }
-
-    if (decimalRepresentation(firstTestArray1) != firstTestNumber1)
-    {
-        printf("Error test number 1\n");
-        return false;
-    }
-
-    if (decimalRepresentation(firstTestArray2) != firstTestnumber2)
-    {
-        printf("Error test number 1\n");
-        return false;
-    }
-
-    int secondTestNumber1 = -5;
-    int secondTestArray1[NUMBER_OF_DIGITS] = { 0 };
-
-    int secondTestnumber2 = -10;
-    int secondTestArray2[NUMBER_OF_DIGITS] = { 0 };
-
-    binaryRepresentation(secondTestNumber1, secondTestArray1);
-    binaryRepresentation(secondTestnumber2, secondTestArray2);
-    if (secondTestArray1[5] != 0 || secondTestArray2[4] != 0 || secondTestArray2[7] != 0)
-    {
-        printf("Error test number 2\n");
-        return false;
-    }
-
-    int secondTestSumArray[NUMBER_OF_DIGITS] = { 0 };
-    columnAddition(secondTestArray1, secondTestArray2, secondTestSumArray);
-    if (secondTestSumArray[4] != 0 || secondTestSumArray[5] != 0 || secondTestSumArray[6] != 0)
-    {
-        printf("Error test number 2\n");
+        printf("Ошибка в тесте номер 2\n");
         return false;
     }
 
@@ -124,7 +108,7 @@ static bool test(void)
 
 static void printArray(int* const array)
 {
-    for (size_t i = 0; i < NUMBER_OF_DIGITS; ++i)
+    for (size_t i = 0; ++i < NUMBER_OF_DIGITS;)
     {
         printf("%d", array[i]);
     }
@@ -141,25 +125,15 @@ int main(void)
     }
 
     int number1 = 0;
-    printf("Введите первое число, меньшее 256 и большее -256 : ");
+    printf("Введите первое число: ");
     scanf_s("%d", &number1);
     int array1[NUMBER_OF_DIGITS] = { 0 };
-    if ((number1 > LIMITATION) || (number1 < -LIMITATION))
-    {
-        printf("Первое число не входит в заданный диапазон");
-        return BEYOND_LIMIT;
-    }
-
+  
     int number2 = 0;
-    printf("Введите второе число, меньшее 256 и большее -256 : ");
+    printf("Введите второе число: ");
     scanf_s("%d", &number2);
     int array2[NUMBER_OF_DIGITS] = { 0 };
-    if ((number2 > LIMITATION) || (number2 < -LIMITATION))
-    {
-        printf("Второе число не входит в заданный диапазон");
-        return BEYOND_LIMIT;
-    }
-
+  
     binaryRepresentation(number1, array1);
     printf("Первое число в дополнительном коде в двоичном представлении ");
     printArray(array1);
@@ -169,17 +143,9 @@ int main(void)
     printArray(array2);
 
     int sumArray[NUMBER_OF_DIGITS] = { 0 };
-    columnAddition(array1, array2, sumArray);
+    binarySum(array1, array2, sumArray);
     printf("Сумма двух чисел в двоичном представлении ");
     printArray(sumArray);
 
-    int decimalRepresentation1 = 0;
-    decimalRepresentation1 = decimalRepresentation(array1);
-    decimalRepresentation1 = number1 < 0 ? decimalRepresentation1 - 256 : decimalRepresentation1;
-
-    int decimalRepresentation2 = 0;
-    decimalRepresentation2 = decimalRepresentation(array2);
-    decimalRepresentation2 = number2 < 0 ? decimalRepresentation2 - 256 : decimalRepresentation2;
-
-    printf("Сумма двух чисел в десятичном представлении %d", decimalRepresentation1 + decimalRepresentation2);
+    printf("Сумма двух чисел в десятичном представлении %d", decimalRepresentation(array1) + decimalRepresentation(array2));
 }
