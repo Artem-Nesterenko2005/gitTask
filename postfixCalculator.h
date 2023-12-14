96931dd5112db69588b54a33d78ee722855d9f5e
Artem-Nesterenko2005 <144232708+Artem-Nesterenko2005@users.noreply.github.com>
2023-12-14 23:02:13 +0500

    Delete postfixCalculator.h

--- postfixCalculator.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-//performing multiplication, addition, subtraction, division operations
-int transactionProcessing(char symbol, int number1, int number2);
-
-//calculator for calculating the result of an expression in postfix form
-int postfixCalculator(char string[], int length);
-
