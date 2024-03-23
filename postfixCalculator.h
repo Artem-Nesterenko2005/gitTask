00411459c01903d4cfa4477362a7b2edac525166
Artem-Nesterenko2005 <144232708+Artem-Nesterenko2005@users.noreply.github.com>
2024-03-23 16:32:36 +0200

    Delete postfixCalculator.h

--- postfixCalculator.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-//create a row based on user data
-char* readString(int* const errorCode);
-
-//postfix calculator 
-int postfixCalculator(const char* const string, const size_t length, int* const errorCode);
